e40690ea5b21c99064264ea8906309c2efc9cf6e
faresh9 <93577791+faresh9@users.noreply.github.com>
2023-08-28 20:13:42 +0300

    c

--- src/context/HoroscopeContext.js
+++ src/context/HoroscopeContext.js
@@ -1,3 +1,15 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const HoroscopeContext = createContext();
+
+
+const HoroscopeProvider = props =>{
+  const 
+  return(
+    <HoroscopeContext.Provider>
+    {props.children}
+  </HoroscopeContext.Provider>
+  );
+}
+
+export default HoroscopeProvider;
