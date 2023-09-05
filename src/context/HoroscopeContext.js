66e852ba7115b38c7b7956aac695927d59951252
faresh9 <93577791+faresh9@users.noreply.github.com>
2023-09-05 14:48:26 +0300

    second

--- src/context/HoroscopeContext.js
+++ src/context/HoroscopeContext.js
@@ -1,15 +1,14 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
 
 export const HoroscopeContext = createContext();
 
-
-const HoroscopeProvider = props =>{
-
-  return(
-    <HoroscopeContext.Provider>
-    {props.children}
-  </HoroscopeContext.Provider>
+const HoroscopeProvider = (props) => {
+  const [currentSign, setCurrentSign] = useState("Cancer");
+  return (
+    <HoroscopeContext.Provider value={{ currentSign, setCurrentSign }}>
+      {props.children}
+    </HoroscopeContext.Provider>
   );
-}
+};
 
 export default HoroscopeProvider;
